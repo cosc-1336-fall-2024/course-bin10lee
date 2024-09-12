666a2c76c6efa21f623325461bb2707dc4bbd53b
bin10lee <youngbin.lee@g.austincc.edu>
2024-09-12 05:26:12 +0000

    HW 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,11 @@
 #
+import decisions
+def main():
+    option=int(input ("Enter Option:"))
+    total=int(input("Enter Total: "))
+
+    result1=decisions.get_options_ratio(option, total)
+    result2= decisions.get_faculty_rating(result1)
+    print ("Ratio: ", )
+
+
