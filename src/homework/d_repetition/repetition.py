0e7c029222214b9e1be4e50cfca21e01e99fa791
bin10lee <youngbin.lee@g.austincc.edu>
2024-09-21 21:30:58 +0000

    Homework 3

--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,17 @@
-#
+#factorial function using a for range loop
+def get_factorial(num1):
+    total_x=1
+    for x in range(num1, 0, -1):
+        total_x=tptal_x*x
+    return total_x
+
+def sum_odd_numbers(num2):
+    y=1
+    total_y=0
+    while y<=num2:
+        if y%2==1:
+            total_y=total_y+y
+        y=y+1
+    return total_y
+    
+    
--- tests/homework/d_repetition/tests_repetition.py
+++ tests/homework/d_repetition/tests_repetition.py
@@ -1,1 +1,16 @@
 #
+import unittest
+from src.homework.d_repetition.repetition import get_factorial
+from src.homework.d_repetition.repetition import sum_odd_numbers
+class Test_Config(unittest.TestCase):
+    def test_get_factorial(self):
+        self.assertEqual(24, get_factorial(4))
+        self.assertEqual(120, get_factorial(5))
+        self.assertEqual(720, get_factorial(6))
+
+    def test_sum_odd_numbers(self):
+        self.assertEqual(16, sum_odd_numbers(7))
+        self.assertEqual(25, sum_odd_numbers(9))
+        self.assertEqual(25, sum_odd_numbers(10))
+        self.assertEqual(49, sum_odd_numbers(14))
+        self.assertEqual(625, sum_odd_numbers(50))
