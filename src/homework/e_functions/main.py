f2a67a889bc09f2b186383e6df016c93bde5617c
bin10lee <youngbin.lee@g.austincc.edu>
2024-09-28 19:10:13 +0000

    Homework 4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,12 @@
 #
+import value_return
+def main ():
+    hours=value_return.get_hour(3800)
+    minutes =value_return.get_minutes(3800)
+    seconds= value_return.get_seconds(3800)
+    print("0", hours, ":", "0", minutes, ":", seconds)
+    
+main()
+
+
+    
