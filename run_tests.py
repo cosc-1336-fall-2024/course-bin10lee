2cfa5dc30894263c5b03cf8d976a92bb48b536c0
bin10lee <youngbin.lee@g.austincc.edu>
2024-10-05 20:47:50 +0000

    Homework5

--- run_tests.py
+++ run_tests.py
@@ -1,10 +1,11 @@
 import unittest
+from tests.homework.h_strings import tests_strings
 '''
 the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 #from tests.examples.b_input_process_output import tests_input_process_output
+suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
 from tests.homework.e_functions import tests_functions
-
-suite = unittest.TestLoader().loadTestsFromModule(tests_functions)
+suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,25 @@
 #
+import strings
+def main():
+    print("MENU|1.Hamming Distance|2.Dna Complement|3.Exit")
+
+    option=int(input("Please enter your selection: "))
+
+    if option==1:
+            dna1=input("Please enter DNA1: ")
+            dna2=input("Please enter DNA2: ")
+            result=strings.get_hamming_distance(dna1, dna2)
+            print("Result: ", result)
+
+    elif option==2:
+            dna=input("Please enter DNA:")
+            result1=strings.get_dna_complement(dna)
+            print("Result: ", result1)
+        
+    elif option==3:
+            print("Exit")
+            exit()
+
+main()
+
+
